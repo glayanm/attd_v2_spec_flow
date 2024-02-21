d637eb41fd9ff58fc4f2fc00c6552ab5fa08108b
YuAn <anm821@gmail.com>
2024-02-21 15:14:06 +0800

    Add Homework.feature

---
+++ attd_v2_spec_flow/Features/Homework.feature
@@ -0,0 +1,5 @@
+﻿Feature: Homework
+	Simple calculator for adding two numbers
+
+Scenario: 環境測試
+When 環境測試
---
+++ attd_v2_spec_flow/Steps/HomeworkStepDefinition.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace attd_v2_spec_flow.Steps;
+
+[Binding]
+public class HomeworkStepDefinition
+{
+    [When(@"環境測試")]
+    public void When環境測試()
+    {
+        Assert.Pass();
+    }
+}
